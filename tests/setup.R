8af1d5a9f4a689a66a52357008d56e72fcbbbfff
Anthony Damico <ajdamico@gmail.com>
2017-08-23 21:42:36 -0400

    pnad 50%

--- tests/setup.R
+++ tests/setup.R
@@ -6,8 +6,8 @@ pnad_cat <-
 	get_catalog( "pnad" ,
 		output_dir = file.path( getwd() ) )
 
-# sample 60% of the records
-which_records <- sample( seq( nrow( pnad_cat ) ) , round( nrow( pnad_cat ) * 0.60 ) )
+# sample 50% of the records
+which_records <- sample( seq( nrow( pnad_cat ) ) , round( nrow( pnad_cat ) * 0.50 ) )
 
 # always sample year == 2011
 pnad_cat <- unique( rbind( pnad_cat[ which_records , ] , subset( pnad_cat , year == 2011 ) ) )
