28fbf6746045caf3b0810ce53ddf8e9b2ca1565a
Anthony Damico <ajdamico@gmail.com>
2017-09-14 08:58:38 -0400

    pnad 2008 removal

--- tests/setup.R
+++ tests/setup.R
@@ -13,6 +13,6 @@ which_records <- sample( seq( nrow( pnad_cat ) ) , round( nrow( pnad_cat ) * 0.5
 pnad_cat <- unique( rbind( pnad_cat[ which_records , ] , subset( pnad_cat , year == 2011 ) ) )
 
 # never sample year == 2008 on travis because it's too big
-if( .Platform$OS.type != 'windows' ) pnad_cat <- subset( pnad_cat , year == 2008 )
+if( .Platform$OS.type != 'windows' ) pnad_cat <- subset( pnad_cat , year != 2008 )
 
 lodown( "pnad" , pnad_cat )
